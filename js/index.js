5cd958d0d11591bceab433a18fcba4ad1b14976e
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2020-05-07 07:58:01 -0700

    not yet mvp

--- js/index.js
+++ js/index.js
@@ -40,3 +40,57 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+//  Task 1: Create selectors to point your data into elements
+//  Create selectors by using any of the DOM element's methods
+//  Note that IDs have been used on all images. Use the IDs to update src path content
+
+//  Task 2: Update the HTML with the JSON data
+//  Remember, NO direct updating of the HTML source is allowed.
+//  Using your selectors, update the content to match the example file.
+//  Remember to update the src attributes on images
+
+// Navigation update from JSON data to HTML
+let navigation = document.querySelectorAll('a');
+
+
+
+navigation[0].textContent = siteContent['nav']['nav-item-1'];
+navigation[1].textContent = siteContent['nav']['nav-item-2'];
+navigation[2].textContent = siteContent['nav']['nav-item-3'];
+navigation[3].textContent = siteContent['nav']['nav-item-4'];
+navigation[4].textContent = siteContent['nav']['nav-item-5'];
+navigation[5].textContent = siteContent['nav']['nav-item-6'];
+
+
+// Call To Action
+let callToActionTitle = document.querySelector('h1');
+
+callToActionTitle.textContent = siteContent['cta']['h1'];
+
+
+//CTA button
+let callToActionButton = document.querySelector("button");
+
+callToActionButton.textContent = siteContent['cta']['button'];
+
+
+//CTA Image
+let callToActionImage = document.querySelector('#cta-img');
+callToActionImage.setAttribute('src', siteContent['cta']['img-src'])
+
+
+//Main Content
+let mainContentFeatures = document.querySelector()
+
+
+
+
+
+
+//  Task 3: Add new content
+//  Change the color of the navigation text to be green.
+//  Utilize .appendChild() and .prepend() to add two new items to the navigation system. You can call them whatever you want.
+//  Check your work by looking at the original html in the browser
